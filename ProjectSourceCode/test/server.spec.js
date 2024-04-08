b5b7074b61d14c14c141a0caadf4763f23af72c9
linneajones18 <linnea.jones@colorado.edu>
2024-04-07 18:19:58 -0600

    added /register positive and negative test cases

--- ProjectSourceCode/test/server.spec.js
+++ ProjectSourceCode/test/server.spec.js
@@ -30,3 +30,46 @@ describe('Server!', () => {
 // *********************** TODO: WRITE 2 UNIT TESTCASES **************************
 
 // ********************************************************************************
+
+describe('Testing Register API', () => {
+  // Positive Testcase :
+  // API: /register
+  // Input: {username: 'johndoe', password: 'jd123'}
+  // Expect: res.status == 200 and res.body.message == 'Success'
+  // Result: This test case should pass and return a status 200 along with a "Success" message.
+  // Explanation: The testcase will call the /register API with the following input
+  // and expects the API to return a status of 200 along with the "Success" message.
+  it('positive : /register', done => {
+    chai
+    .request(server)
+    .post('/register')
+    .send({username: 'johndoe', password: 'jd123'})
+    .end((err, res) => {
+      expect(res).to.have.status(200);
+      expect(res.body.message).to.equals('Success');
+      done();
+    });
+  });
+
+  // Negative Testcase :
+  // API: /register
+  // Input: {username: 10, password: 'hey'}
+  // Expect: res.status == 400 and res.body.message == 'Invalid input'
+  // Result: This test case should pass and return a status 400 along with a "Invalid input" message.
+  // Explanation: The testcase will call the /register API with the following invalid inputs
+  // and expects the API to return a status of 400 along with the "Invalid input" message.
+  it('Negative : /register. Checking invalid name', done => {
+    chai
+      .request(server)
+      .post('/register')
+      .send({username: 10, password: 'hey'})
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res.body.message).to.equals('Invalid input');
+        done();
+      });
+  });
+});
+
+
+//ADD 2 MORE TEST CASES ON APIs OTHER THAN REGISTER
