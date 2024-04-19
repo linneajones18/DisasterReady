62f94f22900ac9542aef9c31f74551813d283032
linneajones18 <linnea.jones@colorado.edu>
2024-04-19 16:37:40 -0600

    profile page mostly done - needs tests written

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     email VARCHAR(50) PRIMARY KEY,
     password CHAR(60) NOT NULL,
-    name VARCHAR(50),
+    name VARCHAR(40),
     location VARCHAR(50),
     bio VARCHAR(200)
 );
